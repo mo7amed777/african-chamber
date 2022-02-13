e7a2a88d48a45f723d25ba1d49e07dfa398c891e
mo7amed777 <mohamed010279316@gmail.com>
2022-02-13 21:22:55 +0200

    jh

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,1 +1,12 @@
-platform :ios, '9.0'
+# Uncomment the next line to define a global platform for your project
+
+# platform :ios, '9.0'
+
+target 'testing_gowtham' do
+
+    use_frameworks!
+    
+    
+    pod 'Firebase/Core'
+    pod 'Firebase/Firestore'
+    end
